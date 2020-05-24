e5034867acfebc81cbc88e78e9affb3e62ecae34
John Leavell <johnleavell@gmail.com>
2020-05-23 22:27:26 -0500

    first commit & tested data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      users: [],
+      balloons: [],
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetchUsers()
+    this.fetchBalloons()
+  }
+
+
+  fetchUsers = () => {
+    fetch(`http://localhost:3000/users`)
+    .then( resp => resp.json())
+    .then( users => 
+      this.setState({
+        users: users
+      }))
+  }
+
+  fetchBalloons = () => {
+    fetch(`http://localhost:3000/balloons`)
+    .then( resp => resp.json())
+    .then( balloons => 
+      this.setState({
+        balloons: balloons
+      }))
+  }
+
+
+  render() {
+    console.log(this.state);
+    
+    return (
+      <div>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
